65bf7b8ccd7d44266e1f1a1b04bf24459d2fbd2d
JhArantes <joaohenriqueavieira@gmail.com>
2024-03-05 13:37:01 -0300

    Refatoração total do projeto, inplementacoes: Palavra pegada em uma API, e outras melhorias na logica

--- Beginner_Projects/find_word.py
+++ Beginner_Projects/find_word.py
@@ -1,63 +1,44 @@
-from random import choice #escolher lista
-import os #limpar terminal
+import requests
+import os
+from random import choice 
 
-# Controle palavras
-list_secrets_words = ['apple','working', 'cavalo', 'computador', 'microfone']
-palavra_secreta = choice(list_secrets_words)
-user_letras = []
-
+r = requests.get('https://www.randomlists.com/data/words.json').json()
+word = choice(r['data']).upper()
 
 print('-=' * 20)
-print('Vamos jogar um jogo!!\nFale letras e direi se ela existe na palavra secreta ou não')
+print('Lets go play one game!!\nSay letters that you think are in the secret word!!')
 print('-=' * 20)
 
-# Main loop
-contador = 0
+correct_letter = []
+score = 0
 while True:
-    contador += 1
+    user_letter = input('\nGuess letter: \n').upper().strip()
     
-    #pega letra
-    letra_user = input("\nDigite uma letra: ").lower().strip()
-    
-    if letra_user == palavra_secreta:
+    if user_letter == word:
         break
-    # Apenas 1 
-    if len(letra_user) > 1:
-        print('Passe apenas uma letra')
+    if len(user_letter) > 1 or not user_letter.isalpha():
+        print('Please, valid entry')
         continue
-    if letra_user in user_letras:
-        print('Está letra já foi digitada')
+    if user_letter in correct_letter:
+        print('This letter has already been typed')
         continue
     
-    
-    user_letras.append(letra_user)
-    print('Secret Word: ', end='')
-    for i in palavra_secreta:
+    if user_letter in word:
+        correct_letter.append(user_letter)
+        print('\nCurrent Word:', end=' ')
+        for i in word:
+            if i in correct_letter:
+                print(i, end='')
+            else:
+                print('-', end='')
+    else:
+        print('Incorrect guess!')
         
-        if i in user_letras:
-            print(i, end='')
-        elif i not in user_letras:
-            print('*', end='')
-            
-    os.system('cls')
+    if set(word) <= set(correct_letter):
+        break
+    score += 1
+
 
 os.system('cls')
 
-print('Você Ganhou!! com ', contador,' tentativas')
-
-
-
-'''
-    for p, i in enumerate(palavra_secreta):
-        if i == letra_user:
-            palavra_user[p] = i
-        
-        
-
-    visao_palavra = ''.join(palavra_user)
-    print(visao_palavra)
-    contador += 1
-    
-    if palavra_secreta == palavra_user:
-        break
-'''  
+print('\nCongratulations! You won with', score,'attempt!!')
